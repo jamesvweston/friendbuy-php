e24f83ec9c71613d33a2e5783c523bec14683e92
James Weston <james@caravana.rentals>
2017-01-16 16:10:26 -0500

    Adding framework for php unit testing

--- src/tests/CustomerTests.php
+++ src/tests/CustomerTests.php
@@ -12,6 +12,7 @@ class CustomerTests extends \PHPUnit_Framework_TestCase
     public function testStuff ()
     {
         $client                     = FriendBuyConfigFactory::getFromEnv();
+
         print_r($client->customerApi->index()->jsonSerialize());
     }
 }
